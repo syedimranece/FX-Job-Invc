461da5a8b326b2f30857cd8a461941b0c51e56ba
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-09-09 16:01:57 +0530

    Added echo statements

--- fx_job_invoke_script.sh
+++ fx_job_invoke_script.sh
@@ -9,4 +9,9 @@ TAGS=$5
 SUITES=$6
 CATEGORIES=$7
 
+
+echo "user=${FX_USER}" 
+echo "region=${REGION}"
+echo "jobid=${FX_JOBID}"
+
 curl --header "Content-Type: application/json;charset=UTF-8" -X POST -u "${FX_USER}":"${FX_PWD}" https://cloud.fxlabs.io/api/v1/runs/job/"${FX_JOBID}"?region="${REGION}"&tags="${TAGS}"&suites="${SUITES}"&categories="${CATEGORIES}"
