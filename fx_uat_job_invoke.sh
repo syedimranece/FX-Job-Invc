343cdbe7ab3a4e766faee89b778883b238b09205
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-09-10 08:48:48 +0530

    Update fx_uat_job_invoke.sh

--- fx_uat_job_invoke.sh
+++ fx_uat_job_invoke.sh
@@ -16,7 +16,7 @@ echo "jobid=${FX_JOBID}"
 
 #curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}" http://localhost:8080/api/v1/runs/job/"${FX_JOBID}"
 
-response=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -w "%{http_code}\n" -u FXLabs//admin@fxlabs.io:admin123$ http://13.56.210.25/api/v1/runs/job/8a80802665c150160165c1779b343761)
+response=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -w "%{http_code}\n" -u "${FX_USER}":"${FX_PWD}"  http://13.56.210.25/api/v1/runs/job/"${FX_JOBID}")
 if [ "$response" == "200" ]
 then
  exit 1
