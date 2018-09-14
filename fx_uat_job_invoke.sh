297853c0631571b813e23c78d0d4a7c0b80033b2
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-09-14 19:06:03 +0530

    Update fx_uat_job_invoke.sh

--- fx_uat_job_invoke.sh
+++ fx_uat_job_invoke.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 # Begin
 
 FX_USER=$1
@@ -14,10 +14,64 @@ echo "user=${FX_USER}"
 echo "region=${REGION}"
 echo "jobid=${FX_JOBID}"
 
-#curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}" http://localhost:8080/api/v1/runs/job/"${FX_JOBID}"
 
-response=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -w "%{http_code}\n" -u "${FX_USER}":"${FX_PWD}"  https://cloud.fxlabs.io/api/v1/runs/job/"${FX_JOBID}")
-if [ "$response" == "" ]
+runId=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}"  https://cloud.fxlabs.io/api/v1/runs/job/${FX_JOBID}?region=${REGION} | jq -r '.["data"]|.id')
+
+echo "runId =" $runId
+if [ -z "$runId" ]
 then
- exit 1
+	  echo "RunId = " "$runId"
+          echo "Invalid runid"
+          exit 1
 fi
+
+
+taskStatus="WAITING"
+echo "taskStatus = " $taskStatus
+
+
+
+while [ "$taskStatus" == "WAITING" -o "$taskStatus" == "PROCESSING" ]
+	 do
+		sleep 5
+		 echo "Checking Status...."
+		  taskStatus=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X GET -u "${FX_USER}":"${FX_PWD}"  https://cloud.fxlabs.io/api/v1/runs/${runId} | jq -r '.["data"]|.task.status')
+		
+		#echo "temp response = " $runResponse
+		#runResponse_=$runResponse
+		
+		#jq -n --argjson data "$runResponse" '$data.data.task.status'
+               
+				
+		echo "taskStatus = " $taskStatus
+
+		if [ "$taskStatus" == "COMPLETED" ];then
+                  	 passPercent=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X GET -u "${FX_USER}":"${FX_PWD}"  https://cloud.fxlabs.io/api/v1/runs/${runId} | jq -r '.["data"]|.task.totalTests') 
+			echo "TotalTest =" "$passPercent"
+                        
+                	echo "Job run successfully completed"
+                        exit 0
+
+                fi
+	done
+
+if [ "$taskStatus" == "TIMEOUT" ];then 
+ exit 1
+else
+ exit 0
+fi
+
+return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
