f2c589b70507a8f3ca3a85e68b1a36d8e9d5d5ec
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-09-14 07:34:02 +0530

    Update fx_job_invoke_script.sh

--- fx_job_invoke_script.sh
+++ fx_job_invoke_script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/sh
 # Begin
 
 FX_USER=$1
@@ -14,10 +14,40 @@ echo "user=${FX_USER}"
 echo "region=${REGION}"
 echo "jobid=${FX_JOBID}"
 
-#curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}" http://localhost:8080/api/v1/runs/job/"${FX_JOBID}"
 
-response=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -w "%{http_code}\n" -u "${FX_USER}":"${FX_PWD}"  https://cloud.fxlabs.io/api/v1/runs/job/"${FX_JOBID}")
-if [ "$response" == "" ]
+runId=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}"  https://cloud.fxlabs.io/api/v1/runs/job/${FX_JOBID}?region=${REGION} | jq -r '.["data"]|.id')
+
+if [ -z "$runId" ]
+then
+	 echo "RunId = " "$runId"
+        echo "Invalid runid"
+fi
+
+
+status=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X GET -u "shoukath@fxlabs.io":"shoukath@fxlabs.io"  https://cloud.fxlabs.io/api/v1/runs/${runId} | jq -r '.["data"]|.task.status')
+
+
+echo "status = " $status
+
+
+while [ "$status" = "WAITING"  -o  "$status" = "PROCESSING" ]
+	 do
+		sleep 10
+	status=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X GET -u "shoukath@fxlabs.io":"shoukath@fxlabs.io"  https://cloud.fxlabs.io/api/v1/runs/${runId} | jq -r '.["data"]|.task.status')
+		
+		echo "status = " $status
+
+		if [ $status = "COMPLETED" ];then
+                  
+                	echo "Job run successfully completed"
+                        exit 0
+
+                fi
+	done
+
+if [ $status = "TIMEOUT" ] 
 then
  exit 1
+else
+ exit 0
 fi
