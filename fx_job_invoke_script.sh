2178b4568d5bd3427b123072c5cc35f15ee95fa0
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-09-16 11:11:16 +0530

    Update fx_job_invoke_script.sh

--- fx_job_invoke_script.sh
+++ fx_job_invoke_script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Begin
 
 FX_USER=$1
@@ -15,36 +15,49 @@ echo "region=${REGION}"
 echo "jobid=${FX_JOBID}"
 
 
-runId=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}"  https://cloud.fxlabs.io/api/v1/runs/job/${FX_JOBID}?region=${REGION} | jq -r '.["data"]|.id')
+runId=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}" http://13.56.210.25/api/v1/runs/job/${FX_JOBID}?region=${REGION} | jq -r '.["data"]|.id')
 
+echo "runId =" $runId
 if [ -z "$runId" ]
 then
-	 echo "RunId = " "$runId"
-        echo "Invalid runid"
+	  echo "RunId = " "$runId"
+          echo "Invalid runid"
+	  echo $(curl -k --header "Content-Type: application/json;charset=UTF-8" -X POST -d '{}' -u "${FX_USER}":"${FX_PWD}" http://13.56.210.25/api/v1/runs/job/${FX_JOBID}?region=${REGION})
+          exit 1
 fi
 
 
-status="WAITING"
-echo "status = " "WAITING"
+taskStatus="WAITING"
+echo "taskStatus = " $taskStatus
 
 
-while [ "$status" = "WAITING"  -o  "$status" = "PROCESSING" ]
+
+while [ "$taskStatus" == "WAITING" -o "$taskStatus" == "PROCESSING" ]
 	 do
-		sleep 10
-	status=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X GET -u "${FX_USER}":"${FX_PWD}"  https://cloud.fxlabs.io/api/v1/runs/${runId} | jq -r '.["data"]|.task.status')
-		
-		echo "status = " $status
+		sleep 5
+		 echo "Checking Status...."
 
-		if [ $status = "COMPLETED" ];then
-                  
+		passPercent=$(curl -k --header "Content-Type: application/json;charset=UTF-8" -X GET -u "${FX_USER}":"${FX_PWD}" http://13.56.210.25/api/v1/runs/${runId} | jq -r '.["data"]|.ciCdStatus') 
+                        
+			IFS=':' read -r -a array <<< "$passPercent"
+			
+			taskStatus="${array[0]}"			
+
+			echo "Status =" "${array[0]}" " Success Percent =" "${array[1]}"  " Total Tests =" "${array[2]}" " Time Taken =" "${array[3]}"
+			
+				
+		echo "taskStatus = " $taskStatus
+
+		if [ "$taskStatus" == "COMPLETED" ];then
+                  	 
+                        
                 	echo "Job run successfully completed"
                         exit 0
 
                 fi
 	done
 
-if [ $status = "TIMEOUT" ] 
-then
+if [ "$taskStatus" == "TIMEOUT" ];then 
  exit 1
 else
  exit 0
