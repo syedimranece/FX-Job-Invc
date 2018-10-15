cacc314bd1d8cb3441a0fc300d7384d756a6bc31
syedimranece <43744709+syedimranece@users.noreply.github.com>
2018-10-15 12:50:33 +0530

    Update fx_job_invoke_script.sh

--- fx_job_invoke_script.sh
+++ fx_job_invoke_script.sh
@@ -67,7 +67,7 @@ echo "Task Status = " $taskStatus
 fi
 
 echo $(curl -k --header "Content-Type: application/json;charset=UTF-8" -X GET -u "${FX_USER}":"${FX_PWD}" https://cloud.fxlabs.io/api/v1/runs/${runId})
-exit 1
+exit 0
 
 return 0
 
